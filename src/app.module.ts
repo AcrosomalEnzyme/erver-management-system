2f86e9e201d45fb0f88543858275407cc6cc7c0f
LYL <dianapgrande@gmail.com>
2023-03-02 13:40:52 +0800

    add environment variable conf

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { LocationsModule } from './locations/locations.module';
 import { DeviceModule } from './device/device.module';
-
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, LocationsModule, DeviceModule],
+  imports: [
+    UsersModule,
+    LocationsModule,
+    DeviceModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
